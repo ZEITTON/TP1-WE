82436980746b5fe05803e3e0bc6c1cc91bba0d33
Jordan FONSECA <jordan.fonseca@etuidant.univ-rennes1.fr>
2023-03-01 11:03:00 +0100

    add view.js

--- view.js
+++ view.js
@@ -1,2 +1,21 @@
 
 // Implémenter ici les fonctions paint à ajouter dans chacune des classes du modèle.
+Rectangle.prototype.paint = function(ctx) {
+    ctx.strokeStyle = this.color;
+    ctx.lineWidth = this.tickness;
+    ctx.strokeRect(this.startX, this.startY, this.width, this.height);
+};
+
+Line.prototype.paint = function(ctx) {
+    ctx.strokeStyle = this.color;
+    ctx.lineWidth = this.tickness;
+    ctx.beginPath();
+    ctx.moveTo(this.startX, startY);
+    ctx.lineTo(this.endX, this.endY);
+};
+
+Drawing.prototype.paint = function(ctx) {
+    ctx.fillStyle = 'F0F0F0';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    this.shapArray.forEach(element => element.paint(ctx));
+};
