0995a453529b5d55521c71c8906eb0596c61e14d
Alexandre <alexandre.chaplais@gmail.com>
2023-03-01 20:50:03 +0100

    q12-13

--- controller.js
+++ controller.js
@@ -7,24 +7,62 @@ function Pencil(ctx, drawing, canvas) {
 	this.currColour = '#000000';
 	this.currentShape = 0;
 
+	document.getElementById('butRect').onclick= (_) => this.currEditingMode=editingMode.rect;
+	document.getElementById('butLine').onclick= (_) => this.currEditingMode=editingMode.line;
+	document.getElementById('spinnerWidth').onchange= (e) => this.currLineWidth=e.target.value;
+	document.getElementById('colour').onchange= (e) => this.currColour=e.target.value;
+
 	// Liez ici les widgets à la classe pour modifier les attributs présents ci-dessus.
 
 	new DnD(canvas, this);
 
 	// Implémentez ici les 3 fonctions onInteractionStart, onInteractionUpdate et onInteractionEnd
 	this.onInteractionStart=function(dnd){
-        this.currentShape = new Rectangle();
+
 	}.bind(this);
 
 	this.onInteractionUpdate=function(dnd){
-        this.currentShape = new Rectangle(dnd.xInit, dnd.yInit, this.currColour, this.currLineWidth, dnd.xFinal, dnd.yFinal);
+        if(this.currEditingMode == editingMode.rect){
+            this.currentShape = new Rectangle(dnd.xInit, dnd.yInit, this.currLineWidth, this.currColour, dnd.yFinal - dnd.yInit, dnd.xFinal - dnd.xInit);
+	    } else {
+	        this.currentShape = new Line(dnd.xInit, dnd.yInit, this.currLineWidth, this.currColour, dnd.xFinal, dnd.yFinal);
+	    }
+	    drawing.paint(ctx, canvas);
         this.currentShape.paint(ctx);
     }.bind(this);
 
     this.onInteractionEnd=function(dnd){
-        this.currentShape = new Rectangle(dnd.xInit, dnd.yInit, this.currColour, this.currLineWidth, dnd.xFinal, dnd.yFinal);
-        this.currentShape.paint(ctx);
+        var uuid = generateUUID();
+        //this.currentShape = new Rectangle(dnd.xInit, dnd.yInit, this.currColour, this.currLineWidth, dnd.yFinal - dnd.yInit, dnd.xFinal - dnd.xInit);
+        drawing.shapeArray.set(uuid, this.currentShape);
+        drawing.paint(ctx, canvas);
+        updateShapeList(uuid, this.currentShape);
+        document.getElementById("remove"+uuid).onclick = (event) => remove(drawing, event.currentTarget.id.substring, ctx, canvas);
+        //this.currentShape.paint(ctx);
     }.bind(this);
+
+    function remove(drawing, index, ctx, canvas){
+        console.log(index);
+        drawing.shapeArray.delete(index);
+        document.getElementById('liRemove' + index).remove();
+        drawing.paint(ctx, canvas);
+    }
+
+    function generateUUID(){
+        var d = new Date().getTime();//Timestamp
+        var d2 = ((typeof performance !== 'undefined') && performance.now && (performance.now()*1000)) || 0;//Time in microseconds since page-load or 0 if unsupported
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+            var r = Math.random() * 16;//random number between 0 and 16
+            if(d > 0){//Use timestamp until depleted
+                r = (d + r)%16 | 0;
+                d = Math.floor(d/16);
+            } else {//Use microseconds since page-load if supported
+                r = (d2 + r)%16 | 0;
+                d2 = Math.floor(d2/16);
+            }
+            return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+        });
+    }
 };
 
 
--- view.js
+++ view.js
@@ -20,3 +20,22 @@ Drawing.prototype.paint = function(ctx) {
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     this.shapeArray.forEach(element => element.paint(ctx));
 };
+
+function updateShapeList(index, shape){
+    document.getElementById('shapeList').insertAdjacentHTML('beforeend', toDom(shape, index));
+};
+
+function toDom(shape, index){
+    if(shape && typeof shape=='object'){
+        let innerHtml = `<li id="liRemove${index}">`;
+        if(shape.constructor == Rectangle){
+            innerHtml += '<span style="color:' + shape.color + '">[]</span> Rectangle';
+        }else if(shape.constructor == Line){
+            innerHtml += '<span style="color:' + shape.color + '">/</span> Rectangle';
+        }
+        innerHtml += `<button type="button" class="btn btn-default remove" id="remove${index}">
+                        <span class="glyphicon glyphicon-remove-sign"></span>
+                     </button>`;
+        return innerHtml + '</li>';
+    }
+}
